209b8f2fcd728e38f6bd1544478c7c40f4527c9f
Moha808 <161479288+Moha808@users.noreply.github.com>
2024-05-08 11:30:46 +0100

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm:"480px",
+      md:"760px",
+      lg:"976px",
+      xl:"1440px"
+    },
+    extend: {
+      colors: {
+        'Lightred': 'hsl(0, 100%, 67%)',
+        // Orangeyellow: 'hsl(39, 100%, 56%)',
+        // Greenteal: 'hsl(166, 100%, 37%)',
+        // Cobaltblue: 'hsl(234, 85%, 45%)',
+        // Lightslateblue: 'hsl(252, 100%, 67%)',
+        // Lightroyalblue: 'hsl(241, 81%, 54%)',
+        // Violetblue: 'hsla(256, 72%, 46%, 1)',
+        // Persianblue: 'hsla(241, 72%, 46%, 0)',
+        // White: 'hsl(0, 0%, 100%)',
+        // Paleblue: 'hsl(221, 100%, 96%)',
+        // Lightlavender: 'hsl(241, 100%, 89%)',
+        // Darkgrayblue: 'hsl(224, 30%, 27%)',
+      },
+      fontFamily:{
+        'sans-serif': ['Hanken Grotesk'],
+      },
+    },
+  },
+  plugins: [],
+}
